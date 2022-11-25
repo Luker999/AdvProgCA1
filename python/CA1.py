750deac35e1e5ce501bd487ab4fe6194ae9c9922
Luker999 <mpdoyle@outlook.com>
2022-11-25 11:15:28 +0000

    info added

--- python/CA1.py
+++ python/CA1.py
@@ -1,2 +1,7 @@
 #https://github.com/Luker999/AdvProgCA1
 #Michael Doyle CA1 Adv Prog DBS 2022-11-25 11:13
+
+
+#define a class employee with 
+#attributes name, weeklyhours, rate, overtimeRate, weeklytaxcredit
+# and store these in the constructor
